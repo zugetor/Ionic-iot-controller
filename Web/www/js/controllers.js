1158a8966e04d3a82f4d07187afa85661b98a059
ZuGeTor <14806377+zugetor@users.noreply.github.com>
2018-11-08 00:02:48 +0700

    Joystick DEMO

--- Web/www/js/controllers.js
+++ Web/www/js/controllers.js
@@ -94,7 +94,12 @@ angular.module('starter.controllers', [])
 
 
 .controller('BellCtrl', function($scope, $state,  $stateParams) {
-	
+	 var static = nipplejs.create({
+        zone: document.getElementById('static'),
+        mode: 'static',
+        position: {left: '50%', top: '50%'},
+        color: 'Aqua'
+    });
 })
 
 .controller('viewcamCtrl', function($scope, $state,  $stateParams) {
